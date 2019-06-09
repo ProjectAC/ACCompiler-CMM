ef4369b522a2899529c15c104fb29882c9f9d479
SOL <1846326091@qq.com>
2019-06-09 13:05:59 +0000

    Current

--- Generator/genControl.c
+++ Generator/genControl.c
@@ -11,4 +11,76 @@ void genFunctionDefinition(Context *context, GrammarTree *node)
         GENERATE(1);
         GENERATE(2);
     }
+    else  // error
+    {
+    }
+}
+
+void genCompoundStatement(Context *context, GrammarTree *node)
+{
+    // '{' '}'
+    if (node->num == 2)
+    {
+    }
+    else if (node->num == 3)
+    {
+	    // '{' statement_list '}'
+	    // '{' declaration_list '}'
+    }
+	// '{' declaration_list statement_list '}'
+    else if (node->num == 4)
+    {
+        GENERATE(1);
+        GENERATE(2);
+    }
+}
+
+void genDeclarationList()
+{
+    // declaration
+	// declaration_list declaration
+}
+
+void genDeclarator(Context *context, GrammarTree *node)
+{
+	// direct_declarator
+    if (node->num == 1)
+    {
+        GENERATE(0);
+    }
+    // pointer direct_declarator
+    else  // error
+    {
+    }
+}
+
+void genDirectDeclarator(Context *context, GrammarTree *node)
+{
+    // IDENTIFIER
+    if (node->num == 1)
+    {
+        GENERATE(0);
+    }
+    else if (node->num == 3)
+    {
+        // '(' declarator ')'
+        // direct_declarator '[' ']'
+        // direct_declarator '(' ')'
+        if (compare(node->child[1], '('))
+        {
+            GENERATE(0);
+        }
+        else
+        {
+        }
+    }
+    else if (node->num == 4)
+    {
+        // direct_declarator '[' constant_expression ']'
+        // direct_declarator '(' parameter_type_list ')'
+        // direct_declarator '(' identifier_list ')'
+    }
+    else // error
+    {
+    }
 }
--- Generator/generator.h
+++ Generator/generator.h
@@ -1,9 +1,12 @@
 #ifndef _GENERATOR_H_
 #define _GENERATOR_H_
 
+#define _INSTRUCTION 0
+#define _LABEL 1
+
 #define GENERATE(x) generate(context, node->child[x])
-#define APPEND(x) append(context, x, INSTRUCTION)
-#define LABEL(x) append(context, x, LABEL)
+#define APPEND(x) append(context, x, _INSTRUCTION)
+#define LABEL(x) append(context, x, _LABEL)
 
 #include "../Tree/tree.h"
 #include <stdio.h>
